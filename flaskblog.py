2f644bee514fa467b6b0fa7d9ecccae4b7b8d672
MJ <michaeljefferies08@gmail.com>
2018-07-27 23:35:03 +0100

    Added new post and new app.route decorator ("/home")

--- flaskblog.py
+++ flaskblog.py
@@ -4,17 +4,23 @@ app = Flask(__name__)
 # sample database call info
 posts = [
     {
-        'Author': "Michael Jefferies",
+        "author": "Michael Jefferies",
         "title": "Blog Post 1",
-        "content: First post content",
+        "content": "First post content",
         "date_posted": "April 21,2018"
+    },
+    {
+        "author": "Some Random Guy",
+        "title": "lol who's gonna look at this anyway",
+        "content": "Second post content",
+        "date_posted": "July 27,2018"
     }
 ]
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html",posts="posts")
+    return render_template("home.html",posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html",title="About")
